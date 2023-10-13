6c7ddaa3ad1ed04ca5914e9a125eec7c46c131cc
David Bittencourt <devp.dade@gmail.com>
2023-10-13 15:24:41 -0300

    OrderStatus enum

--- src/main/java/com/davidprojects/course/config/TestConfig.java
+++ src/main/java/com/davidprojects/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.davidprojects.course.entities.Order;
 import com.davidprojects.course.entities.User;
+import com.davidprojects.course.entities.enums.OrderStatus;
 import com.davidprojects.course.repositories.OrderRepository;
 import com.davidprojects.course.repositories.UserRepository;
 
@@ -35,9 +36,9 @@ public class TestConfig implements CommandLineRunner{
 		
 		//T = time e Z = UTC
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1); 
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2); 
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1); 
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2); 
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 		
