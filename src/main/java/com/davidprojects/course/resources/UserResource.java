c3444457f18396449236eb7908ea1961762caffd
David Bittencourt <devp.dade@gmail.com>
2023-10-17 00:49:28 -0300

    User insert

--- src/main/java/com/davidprojects/course/resources/UserResource.java
+++ src/main/java/com/davidprojects/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.davidprojects.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.davidprojects.course.entities.User;
 import com.davidprojects.course.services.UserService;
@@ -34,5 +38,19 @@ public class UserResource {
 		
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	//Post
+	//Header Content-Type application/json
+	@PostMapping
+	public ResponseEntity <User> insert(@RequestBody User obj){
+		 obj = service.insert(obj);
+		 
+		 URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				 .buildAndExpand(obj.getId()).toUri();
+		 
+		 //Para retornar O cod 201 - Created precisa como parametro a uri do obj recém criado, para isso criamos um URI na linha acima
+		 return ResponseEntity.created(uri).body(obj);
+		 
 	}
 }
